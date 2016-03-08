3596b65484dc31edb78cb54aa012d08353b67aa9
masbicudo <masbicudo@gmail.com>
2016-03-08 00:36:48 -0300

    Extension method to compile any type of System.Linq.Expression.

--- Lambda2Js/LambdaExpressionExtensions.cs
+++ Lambda2Js/LambdaExpressionExtensions.cs
@@ -38,5 +38,35 @@ namespace Lambda2Js
 
             return $"function({string.Join(",", visitor.UsedScopeMembers)}){{return {visitor.Result};}}";
         }
+
+        /// <summary>
+        /// Compiles an expression to JavaScript code.
+        /// </summary>
+        /// <param name="expr">Expression to compile to JavaScript.</param>
+        /// <param name="options">
+        /// Conversion options:
+        /// What extensions to use (i.e. StaticStringMethods, StaticMathMethods, or any other custom extensions).
+        /// Options not supported by this overload of `CompileToJavascript`:
+        /// Include only the body of the lambda (supported only in the LambdaExpression overload of this method);
+        /// Use a single scope parameter (supported only in the LambdaExpression overload of this method).
+        /// </param>
+        /// <returns>JavaScript code represented as a string.</returns>
+        public static string CompileToJavascript([NotNull] this Expression expr, JavascriptCompilationOptions options = null)
+        {
+            if (expr == null)
+                throw new ArgumentNullException(nameof(expr));
+
+            options = options ?? JavascriptCompilationOptions.DefaultOptions;
+
+            var visitor =
+                new JavascriptCompilerExpressionVisitor(
+                    null,
+                    options.Extensions,
+                    options);
+
+            visitor.Visit(expr);
+
+            return visitor.Result;
+        }
     }
 }
