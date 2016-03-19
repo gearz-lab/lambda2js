fd0b4d63e4bc2cc33e492ab979afb2d00d548787
masbicudo <masbicudo@gmail.com>
2016-03-19 15:02:22 -0300

    Included project to test package from NuGet.

---
+++ Test.NuGet.Net-v4.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Lambda2Js;
+
+namespace Test.NuGet.Net_v4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int>> expr = x => 1024 + x;
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(0, ScriptVersion.Es60));
+            //Assert.AreEqual(@"x=>1024+x", js);
+        }
+    }
+}
