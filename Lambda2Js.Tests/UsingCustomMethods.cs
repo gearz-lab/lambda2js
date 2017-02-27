d020ac70767a2286b7e2c40a3a37e829511907d3
aviv86 <aviv@ayende.com>
2017-02-27 12:09:43 +0200

    Reverting changes in JavascriptCompilerExpressionVisitor because we can do the same work more easily using an extension

--- Lambda2Js.Tests/UsingCustomMethods.cs
+++ Lambda2Js.Tests/UsingCustomMethods.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using Lambda2Js.Properties;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lambda2Js.Tests
@@ -15,12 +14,74 @@ namespace Lambda2Js.Tests
         {
             Expression<Func<JSArray, object>> expr = array => array.RemoveAt(2);
 
+            var extension = new CustomMethods();
             var js = expr.CompileToJavascript(
                 new JavascriptCompilationOptions(
                     JsCompilationFlags.BodyOnly | JsCompilationFlags.ScopeParameter,
-                    new[] { new LinqMethods(), }));
+                    new LinqMethods(), extension));
 
-            Assert.AreEqual("array.splice(2,1)", js);
+            Assert.AreEqual("array.splice(arg_0, 1)", js);
+            Assert.AreEqual(2, ((ConstantExpression)extension.Parameters["arg_0"]).Value);
+        }
+
+        public class JavascriptMethodNameAttribute : Attribute
+        {
+            public string Name { get; }
+
+            public object[] PositionalArguments { get; set; }
+
+            public JavascriptMethodNameAttribute(string name)
+            {
+                Name = name;
+            }
+        }
+
+        public class CustomMethods : JavascriptConversionExtension
+        {
+            public Dictionary<string, object> Parameters = new Dictionary<string, object>();
+            public override void ConvertToJavascript(JavascriptConversionContext context)
+            {
+                var methodCallExpression = context.Node as MethodCallExpression;
+
+                var nameAttribute = methodCallExpression?
+                    .Method
+                    .GetCustomAttributes(typeof(JavascriptMethodNameAttribute),false)
+                    .OfType<JavascriptMethodNameAttribute>()
+                    .FirstOrDefault();
+
+                if (nameAttribute == null)
+                    return;
+
+                context.PreventDefault();
+
+                context.Visitor.Visit(methodCallExpression.Object);
+                var javascriptWriter = context.GetWriter();
+                javascriptWriter.Write(".");
+                javascriptWriter.Write(nameAttribute.Name);
+                javascriptWriter.Write("(");
+
+                for (int i = 0; i < methodCallExpression.Arguments.Count; i++)
+                {
+                    var name = "arg_" + Parameters.Count;
+                    if (i != 0)
+                        javascriptWriter.Write(", ");
+                    javascriptWriter.Write(name);
+                    Parameters[name] = methodCallExpression.Arguments[i];
+                }
+                if (nameAttribute.PositionalArguments != null)
+                {
+                    for (int i = methodCallExpression.Arguments.Count;
+                        i < nameAttribute.PositionalArguments.Length;
+                        i++)
+                    {
+                        if (i != 0)
+                            javascriptWriter.Write(", ");
+                        context.Visitor.Visit(Expression.Constant(nameAttribute.PositionalArguments[i]));
+                    }
+                }
+
+                javascriptWriter.Write(")");
+            }
         }
 
         public class JSArray
