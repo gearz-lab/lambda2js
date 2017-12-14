b5e35a042b2ad1c07f3912df3afcd310b507ddc6
Miguel Angelo <masbicudo@gmail.com>
2017-12-14 14:33:04 -0200

    Tests for net40 corrected

--- Test.Net-v4.0/Program.cs
+++ Test.Net-v4.0/Program.cs
@@ -13,10 +13,21 @@ namespace Test.Net_v4._0
             Assert.Default = new MyAssert();
 
             Console.WriteLine("Tests with .NET 4.0");
+            Console.WriteLine("===================");
+            Console.WriteLine("");
+            Console.WriteLine("This tool will run all the tests using the .Net Framework 4.0");
+            Console.WriteLine("It is needed because I could not find the usual test classes");
+            Console.WriteLine("in this version of the framework.");
+            Console.WriteLine("");
+            Console.WriteLine("===================");
+            Console.WriteLine("");
 
             var allTestClasses = typeof(GeneralTests).Assembly.GetTypes()
                 .Where(t => t.GetCustomAttributes<TestClassAttribute>().Any())
                 .ToArray();
+
+            int passed = 0;
+            int total = 0;
 
             foreach (var t in allTestClasses)
             {
@@ -28,14 +39,17 @@ namespace Test.Net_v4._0
 
                 foreach (var mi in methods)
                 {
+                    total++;
                     bool ok = false;
                     try
                     {
                         mi.Invoke(obj, null);
                         ok = true;
                     }
-                    catch (Exception ex)
+                    catch (TargetInvocationException tex)
                     {
+                        var ex = tex.InnerException;
+
                         ok = mi.GetCustomAttributes<ExpectedException>().Where(x => x.Accept(ex)).Any();
 
                         if (!ok)
@@ -47,11 +61,41 @@ namespace Test.Net_v4._0
                             Console.WriteLine($"{ex.GetType().Name}");
                         }
                     }
+                    catch (Exception ex)
+                    {
+                        ok = false;
+                        var prev = Console.ForegroundColor;
+                        Console.ForegroundColor = ok ? ConsoleColor.Green : ConsoleColor.Red;
+                        Console.Write(ok ? "  OK  " : " FAIL ");
+                        Console.ForegroundColor = prev;
+                        Console.WriteLine($"{ex.GetType().Name}: {ex.Message}");
+                    }
+
+                    if (ok)
+                        passed++;
                 }
             }
 
-            Console.WriteLine("Press ENTER to exit");
-            Console.ReadLine();
+            var prev2 = Console.ForegroundColor;
+            Console.WriteLine("");
+            Console.WriteLine("===================");
+            Console.WriteLine("");
+            Console.WriteLine("Tests finished running:");
+            Console.Write("Passed: ");
+            Console.ForegroundColor = passed > 0 ? ConsoleColor.Green : prev2;
+            Console.WriteLine(passed);
+            Console.ForegroundColor = prev2;
+            Console.Write("Failed: ");
+            Console.ForegroundColor = total - passed > 0 ? ConsoleColor.Red : prev2;
+            Console.WriteLine(total - passed);
+            Console.ForegroundColor = prev2;
+            Console.Write("Total: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(total);
+            Console.ForegroundColor = prev2;
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(intercept: true);
         }
     }
 }
