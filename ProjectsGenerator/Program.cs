22dae794e7cb01ccfc0c9fcfb20a26c76afce180
Miguel Angelo <masbicudo@gmail.com>
2017-12-14 23:26:37 -0200

    Better Project Generator

--- ProjectsGenerator/Program.cs
+++ ProjectsGenerator/Program.cs
@@ -6,6 +6,8 @@ namespace ProjectsGenerator
 {
     class Program
     {
+        private static readonly string ProjectName = "Lambda2Js";
+
         static void Main(string[] args)
         {
             Console.WriteLine($"Environment.CurrentDirectory = {Environment.CurrentDirectory}");
@@ -13,19 +15,19 @@ namespace ProjectsGenerator
             // TEST PROJECTS:
             //
             // Test projects are generated from the main multi-targeted test project:
-            // "Lambda2Js.Tests.csproj" file.
+            // "{ProjectName}.Tests.csproj" file.
 
             {
 
                 var l2jsTests = new XmlDocument();
-                l2jsTests.Load("../Lambda2Js.Tests/Lambda2Js.Tests.csproj");
+                l2jsTests.Load($"../{ProjectName}.Tests/{ProjectName}.Tests.csproj");
 
                 var targetsElement = l2jsTests.SelectNodes("//TargetFrameworks");
                 Console.WriteLine(targetsElement[0].InnerText);
                 var targets = targetsElement[0].InnerText.Split(";");
                 foreach (var target in targets)
                 {
-                    if (File.Exists($"../Lambda2Js.Tests/Lambda2Js.Tests.{target}.csproj"))
+                    if (File.Exists($"../{ProjectName}.Tests/{ProjectName}.Tests.{target}.csproj"))
                         continue;
 
                     var l2jsTestNew = (XmlDocument)l2jsTests.Clone();
@@ -33,19 +35,19 @@ namespace ProjectsGenerator
                     var targetElement = l2jsTestNew.CreateElement("TargetFramework");
                     targetElement.InnerText = target;
                     node.ParentNode.ReplaceChild(targetElement, node);
-                    l2jsTestNew.Save($"../Lambda2Js.Tests/Lambda2Js.Tests.{target}.csproj");
+                    l2jsTestNew.Save($"../{ProjectName}.Tests/{ProjectName}.Tests.{target}.csproj");
                 }
 
             }
 
             // SIGNED ASSEMBLY
             //
-            // Signed assembly is generated from the main "Lambda2Js.csproj" file.
+            // Signed assembly is generated from the main "{ProjectName}.csproj" file.
 
             {
 
                 var l2js = new XmlDocument();
-                l2js.Load("../Lambda2Js/Lambda2Js.csproj");
+                l2js.Load($"../{ProjectName}/{ProjectName}.csproj");
 
                 var sig = (XmlDocument)l2js.Clone();
                 var sigMain = sig.SelectSingleNode("//TargetFrameworks").ParentNode;
@@ -53,23 +55,30 @@ namespace ProjectsGenerator
 
                 sigMain.SelectSingleNode("AssemblyVersion").InnerText = $"{ver}.0";
                 sigMain.SelectSingleNode("FileVersion").InnerText = $"{ver}.0";
-                sigMain.SelectSingleNode("PackageId").InnerText = "Lambda2Js.Signed";
+                sigMain.SelectSingleNode("PackageId").InnerText += ".Signed";
                 sigMain.AppendChild(sig.CreateElement("SignAssembly").With(x => x.InnerText = "true"));
-                sigMain.AppendChild(sig.CreateElement("AssemblyOriginatorKeyFile").With(x => x.InnerText = @"..\Lambda2Js.snk"));
-                sigMain.AppendChild(sig.CreateElement("RootNamespace").With(x => x.InnerText = "Lambda2Js"));
-                sigMain.AppendChild(sig.CreateElement("AssemblyName").With(x => x.InnerText = "Lambda2Js.Signed"));
+                sigMain.AppendChild(sig.CreateElement("AssemblyOriginatorKeyFile").With(x => x.InnerText = $@"..\{ProjectName}.snk"));
 
-                foreach (XmlNode doc in sig.SelectNodes("DocumentationFile"))
-                    doc.InnerText = doc.InnerText.Replace("Lambda2Js.xml", "Lambda2Js.Signed.xml");
+                if (sigMain.SelectSingleNode("RootNamespace") == null)
+                    sigMain.AppendChild(sig.CreateElement("RootNamespace").With(x => x.InnerText = $"{ProjectName}"));
 
-                sig.Save($"../Lambda2Js/Lambda2Js.Signed.csproj");
+                if (sigMain.SelectSingleNode("AssemblyName") == null)
+                    sigMain.AppendChild(sig.CreateElement("AssemblyName").With(x => x.InnerText = $"{ProjectName}.Signed"));
+                else
+                    sigMain.SelectSingleNode("AssemblyName").InnerText += ".Signed";
+
+                foreach (XmlNode doc in sig.SelectNodes("//DocumentationFile"))
+                    doc.InnerText = doc.InnerText.Replace($"{ProjectName}.xml", $"{ProjectName}.Signed.xml");
+
+                sig.Save($"../{ProjectName}/{ProjectName}.Signed.csproj");
 
             }
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
     }
+
     static class ObjectExtensions
     {
         public static T With<T>(this T obj, Action<T> action)
